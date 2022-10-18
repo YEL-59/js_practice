d6a8ce4ad037694783ae27b6ead205998a57f3c8
YEL-59 <rokomari144@gmail.com>
2022-10-18 17:02:58 +0600

    #feat:(for loop)

---
+++ js/chapter4(loop).js
@@ -0,0 +1,74 @@
+//loop
+
+// for(var i=1; i<10;i++){
+//     //console.log('tofayel islam');
+//     if(i%2==0){
+//         console.log(i)
+//     }
+// }
+
+//while loop
+//  var i=0;
+//  while(i<10){
+//     console.log('tofayel islam')
+//     i++
+//  }
+
+
+
+
+
+//  var running =true
+
+
+
+// while(running){
+//     var rand =Math.floor(Math.random()*10+1)
+//     var guess=parseInt(prompt('enter a number:'))
+//     if(rand==guess){
+//         console.log('you win guess the right number');
+//         console.log(' your number is' +rand)
+//         running=false
+//break
+//     }else{
+//         console.log('your choice is wrong, your number is' +rand)
+//     }
+// }
+
+// do{
+//     console.log('ok it is ok')
+// }while(running)
+
+
+
+// for(var i=1; i<5;i++){
+//     for(var j=1; j<5;j++){
+//         console.log('j')
+//     }
+//     console.log('\n')
+// }
+
+
+// let n = parseInt(prompt('enter the valu'));
+// let string = "";
+// for (let i = 1; i <= n; i++) {
+
+//   for (let j = 0; j < n - i; j++) {
+//     string += " ";
+//   }
+ 
+//   for (let k = 0; k < i; k++) {
+//     string += "*";
+//   }
+//   string += "\n";
+// }
+// console.log(string);
+
+// var n = parseInt(prompt('enter the valu'));
+// for(var i=1;i<n;i++){
+//     var result=""
+//     for(var j=1;j<i;j++){
+//         result += '*'+''
+//     }
+//     console.log(result)
+// }
