e1bced4ef9d7cd742877f3e86819ea322490472e
YEL-59 <rokomari144@gmail.com>
2022-10-18 18:52:02 +0600

    all avout string

---
+++ js/chapter5(string).js
@@ -0,0 +1,10 @@
+document.write('hey ')
+
+var str='tofayel'
+var n=10
+str3=n.toString()
+str2= String(n)
+console.log(n)
+// how to compare 2 string in javascript ==>there are 2 type 
+//1 is laxicoualgraphical order type.in that type small a -z is highest priority in A-Z where a is small and z is bigger so a=aaaaaZ compare to b=aaaaz the result is a is biger because the last is small z wherevalu is biger ,
+//2 another type is '001'==1 or ''11'=='22' ther are firstly both string are integer then compare the intejer value 
