fad37f47d0475fe0a65418573444ab3f01de6e17
YEL-59 <rokomari144@gmail.com>
2022-10-18 12:58:23 +0600

    #feat:(chapter 3 .done==>if-else,switch,ternary operator)

---
+++ js/chapter3(if-else-switch0.js
@@ -0,0 +1,97 @@
+// var a=10;
+// var b=20;
+
+// a=a+b;
+// b=a-b;
+// a=a-b;
+
+// console.log("value of a is ",a);
+// console.log("value of b is ",b);
+
+//switch case study
+
+// var date= new Date();
+// console.log(date)
+// var today = date.getDay();
+
+
+// switch (today) {
+//         case 0:
+//         console.log("today is saturday");
+//         break;
+//         case 1:
+//         console.log("today is sunday");
+//         break;
+
+//         case 2:
+//         console.log("today is munday");
+//         break;
+
+//         case 3:
+//         console.log("today is tuesday");
+//         break;
+
+//         case 4:
+//         console.log("today is wednesday");
+//         break;
+
+//         case 5:
+//         console.log("today is thursday");
+//         break;
+
+//         case 6:
+//         console.log("today is friday");
+//         break;
+
+
+//     default:
+//         console.log("error 404 ");
+//         break;
+// }
+
+
+//logical operator
+
+// var a=10;
+//  b=20;
+//  c=30;
+//   d="40";
+
+// if(a>b || c>d){
+//     console.log('%d is greter then %d and %d is greter then %d',a,b,c,d);
+// }else{
+//     console.log("at last one condition true")
+//     console.log(typeof(d))
+// }
+
+// if(a>b || c>d){
+//     console.log('%d is greter then %d and %d is greter then %d',a,b,c,d);
+// }else{
+//     console.log("at last one condition false")
+// }
+// var check= !!(a>b)
+// console.log(check)
+
+
+//ternary operator
+
+// var a=prompt("give a number to check odd-even :");
+
+// var str='';
+// if(a%2==0){
+//     console.log("%d is odd!",a);
+//     str='odd!'
+//     console.log(str)
+// }else{
+//     console.log("the value is %d even",a)
+//     str='even'
+//     console.log(str)
+//     // alert("the value is %d is even ",a)
+    
+// }
+
+
+// var result= a%2==0 ? 'even':'odd'
+// console.log(result)
+//condition?"true":"false"...it is use one line statement when it is multiple line that case we use if else switch etc
+
