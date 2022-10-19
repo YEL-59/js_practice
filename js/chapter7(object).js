8311ad18556fb90a1e776bb87bbf633c679d81a8
YEL-59 <rokomari144@gmail.com>
2022-10-19 15:45:36 +0600

    #feat:(array==>simple array methods)

---
+++ js/chapter7(object).js
@@ -0,0 +1,43 @@
+//object
+
+// var obj = {}
+// obj.x=10
+// console.log(obj)
+
+// var point ={
+//     x:10,
+//     y:20
+// }
+//point.y=30
+// point.z=30
+// console.log(point)
+
+
+//accase of object property
+
+var point={
+    x:10,
+    y:20,
+    z:30,
+}
+//for in travers or iterable thats mean each element of object
+for(var i in point){
+    console.log(i)
+    console.log(i + ':'+ point[i])
+}
+
+// delete point.z
+// console.log(point)
+// point.z=30
+// console.log(point)
+// console.log(point.z +point.y)
+// console.log(point['z'])
+// console.log(point.z)
+
+//delet operator 
+
+
+
+
+
+
