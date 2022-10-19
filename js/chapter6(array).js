6710317cc94ab2c43f30a1e4afb76c95a7c9e65c
YEL-59 <rokomari144@gmail.com>
2022-10-19 12:42:32 +0600

    #feat:(chapter6==> all about basic array ,array methods etc)

---
+++ js/chapter6(array).js
@@ -0,0 +1,78 @@
+//array
+
+
+// var arr = [1,2,3,4,5,6]
+// var sum=0;
+// for (var i=0;i<arr.length;i++){
+//     // sum=sum+arr[i]
+//     if(arr[i]%2===0){
+//         console.log(even)
+       
+//     }
+// //console.log(arr[i])
+//     // arr[i]=arr[i] + 2
+// }
+
+// console.log(arr[i])
+
+
+// var b = [1, 2, 3, 4]
+
+// sum2 = 0
+
+// for (i = 0; i < b.length; i++) {
+
+//     if (a[i] % 2 === 0) {
+
+//         sum2 += b[i]
+
+//     }
+
+// }
+
+// console.log(sum2)
+
+//push,unshift an array
+
+
+// var arr=[1,2,3]
+// arr.push(4)
+// arr.unshift(0)
+// arr.splice(5,0,5,6)
+// console.log(arr)
+
+//search element of an array
+
+// var arr=[1,2,3,4,5,6,7,8,9]
+//  var find =5
+// var isfound=false
+
+//  for(var i=0;i<arr.length;i++){
+//     if(arr[i]==find){
+//         console.log('Data found of index '+i)
+//         isfound=true
+//         break
+//     }
+//  }
+
+//  if(!isfound){
+//     console.log('404 error!')
+//  }
+
+//array reverse
+
+// var arr=[1,2,3,4,5,6,7,8,9]
+
+// console.log(arr.reverse())
+// for(var i=n; i>=0;i--){
+//     console.log(arr)
+// }
+
+//join
+
+var menu =['home','about','contact','like']
+var arr=[1,2,3,4,5,6,7,8,9]
+var arr2=menu.concat(arr)
+menu.fill(0)
+console.log(menu.join(' /'))
+console.log(arr2)
