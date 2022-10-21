ed137f3e3fa77e2d8e76ddc3afe2cfd86f48cbc4
YEL-59 <rokomari144@gmail.com>
2022-10-21 15:50:20 +0600

    #scope and closer

---
+++ js/chapter10(scope&closer).js
@@ -0,0 +1,34 @@
+//scope and closer 
+//javascript engine is read code understanding  and javascript code run
+// source code==>parser==>windows tree==>machine code ==>excution
+//************ 
+// js is compiled or interpreted c++ (jit) just in time
+
+function a(){
+    console.log('i am function')
+}
+function b(){
+    console.log('i am function b')
+}
+a()
+
+b()
+//execution context
+
+//global context, context is a container real world  a context is an object create and execution context
+
+var a=10
+console.log(a)
+var b 
+
+console.log(b)
+
+//b=20
+var c=50
+console.log(c)
+
+
+
+
+
+
