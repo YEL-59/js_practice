2a1824bdccaee5f60274a1638b03643bd9e4df6f
YEL-59 <rokomari144@gmail.com>
2022-10-29 17:42:32 +0600

    es6

---
+++ js/chapter15(es6).js
@@ -0,0 +1,28 @@
+// (function (){
+//     var a='tofayel'
+//     console.log(a)
+
+// })()
+
+// console.log(a)
+
+// function add(a,b){
+//     return a+b
+// }
+// console.log(add(2,2))
+
+//arrow function
+
+// let add =(a,b)=>{
+//     return a+b
+ 
+// }
+
+// let add=(a,b)=> a+b
+// console.log(add(45,50))
+// let sqr =(x)=>x*x
+
+function testme(){
+    console.log(this)
+}
+testme()
