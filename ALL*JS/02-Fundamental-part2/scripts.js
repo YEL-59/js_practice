e05473f94144d5b77a32d878203d4146d9fa5a27
Md. Tofayel Islam <tofayelislam19986@gmail.com>
2023-08-05 23:12:02 +0600

    promise concept

--- ALL*JS/02-Fundamental-part2/scripts.js
+++ ALL*JS/02-Fundamental-part2/scripts.js
@@ -200,7 +200,7 @@ while (dice !== 6) {
   dice = Math.trunc(Math.random() * 6) + 1;
   if (dice === 6) console.log('Loop is about to end...');
 }
-*/
+
 const bills = [22, 295, 176, 440, 37, 105, 10, 1100, 86, 52];
 console.log(bills)
 const tips=[];
@@ -378,6 +378,97 @@ console.log(bills);
 //   bills1.push(Math.floor(Math.random() * 1000));
 // }
 // console.log(bills1);
+
+*/
+
+const status = true
+
+
+console.log("Task Number 1")
+
+//promise call
+const promise = new Promise((resolve,reject)=>{
+  setTimeout(()=>{
+    status?resolve("Task Number 2"):reject("failed")
+  },1)
+}
+)
+//promise call end
+promise.then((value)=>{
+  console.log(value)
+}
+).catch((err)=>{
+  console.log(err)
+}
+)
+
+// setTimeout(()=>{
+//   status?console.log("Task Number 2"):console.log("failed")
+ 
+// },1)
+
+
+
+console.log("Task Number 3")
+
+
+
+
+
+//promise Project
+
+const PaymentStatement = true
+const marks=70
+
+const enroll = function  () {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      PaymentStatement
+        ? resolve("Payment Successful")
+        : reject("Payment Failed");
+    }, 1000);
+  });
+}
+
+const progress =function () {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      marks>50
+        ? resolve("Progress Successful")
+        : reject("Progress Failed");
+    }, 1000);
+  });
+}
+const getcertificate = function () {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      marks>50
+        ? resolve("Certificate Successful")
+        : reject("Certificate Failed");
+    }, 1000);
+  });
+}
+
+enroll()
+  .then((value) => {
+    console.log(value);
+    return progress();
+  }
+  )
+  .then((value) => {
+    console.log(value);
+    return getcertificate();
+  }
+  )
+  .then((value) => {
+    console.log(value);
+  }
+  )
+  .catch((err) => {
+    console.log(err);
+  }
+  );
+
 
 
 
